c50814cc742fe5a00b5458f0f489b5f6582704cc
mnalon <marcelo.nalon@engenharia.ufjf.br>
2014-11-28 12:23:41 -0200

    routes to send message and get all messages

--- routes/chat.py
+++ routes/chat.py
@@ -1,13 +1,40 @@
+# -*- coding: utf-8 -*-
+
 __author__ = 'Nalon'
-from flask import render_template, Blueprint,request,Response
+from flask import render_template, Blueprint,request,Response,jsonify,json
+from datetime import datetime
+from collections import deque
+
+queue_messages = deque([])
 
 bp_chat =  Blueprint('bp_chat',__name__)
 
 @bp_chat.route('/chat/message', methods=['POST'])
 def templateSubscription():
     try:
+        json_data = {
+                        "message" : "",
+                        "time_stamp" : ""
+                    }
         request_json = request.json
-        print "message received: %s" %request_json["message"]
+
+        json_data["message"] = request_json["message"].encode('UTF-8')
+        json_data["time_stamp"] = datetime.now().strftime("%d/%m/%y  %H:%M:%S")
+        if len(queue_messages) == 20:
+            queue_messages.popleft()
+        queue_messages.append(json_data)
+        print len(queue_messages)
+        for data in queue_messages:
+            print data
         return Response(status=200)
-    except:
+    except Exception as e:
+        print e.message
         return Response(status=500)
+
+@bp_chat.route('/chat/all', methods=['GET'])
+def templateViewAll():
+    json_messages = {
+        "messages":list(queue_messages)
+    }
+
+    return jsonify(json_messages)
